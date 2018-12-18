8d228f097d09897a5b2e247cf880cc70b6aeb9fa
gokhanDerala <gderala@gmail.com>
2018-12-18 23:23:29 +0300

    canvas

--- canvas.js
+++ canvas.js
@@ -2,3 +2,12 @@ function Canvas2D(){
 	this._canvas = document.getElementById('screen');
 	this._canvasContent = this._canvas.getContext('2d');
 }
+
+
+Canvas2D.prototype.clear = function(){
+	this._canvasContent.clearRect(0, 0, this._canvas.width, this._canvas.height);
+}
+
+Canvas2D.prototype.drawImage = function(image, position){
+	this._canvasContent.drawImage(image, position.x, position.y);
+}
